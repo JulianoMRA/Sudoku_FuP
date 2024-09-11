c6336ddd522c7c4e9f2ade953ac8d3343fede5bd
mgabrielalmeida <gabriel.matias.lg@gmail.com>
2024-09-11 13:11:32 -0300

    attpika

--- sudoku.py
+++ sudoku.py
@@ -110,150 +110,167 @@ def check(tabuleiro, coluna, linha, numero):
         return False
     return True
 
-# Verifica se o nome do arquivo foi passado como argumento:
-if len(sys.argv) != 2:
-    print("Use:\n  - Modo Interativo: python sudoku.py <arquivo_pistas>\n")
-    sys.exit(1)
 
-# Inicializando o tabuleiro e tabuleiroBool
-tabuleiro = [[0 for i in range(9)] for i in range(9)]
-tabuleiroBool = [[True for i in range(9)] for i in range(9)]
+if not 2 <= len(sys.argv) <= 3:
+    print("""A quantidade de arquivos não condiz com nenhum modo de jogo
+            Modo Interativo:
+                Um arquivo.
+            Modo Batch:
+                Dois arquivos.""")
 
-# Capturando o nome do arquivo da linha de comando
-arquivo = sys.argv[1]
+else:
+    # Inicializando o tabuleiro e tabuleiroBool
+    tabuleiro = [[0 for _ in range(9)] for _ in range(9)]
+    tabuleiroBool = [[True for _ in range(9)] for _ in range(9)]
 
-flag2 = True
-# Ler o arquivo de jogadas e preencher o tabuleiro
-flag3 = ler_jogadas(arquivo, tabuleiro)
-if flag3:
-    flag2 = False
-    print("Arquivo de dicas inválido!")
+    # Capturando o nome do arquivo da linha de comando
+    arquivo = sys.argv[1]
 
-# Atualizando o tabuleiroBool para marcar as dicas como imutáveis
-for i in range(9):
-    for j in range(9):
-        if tabuleiro[i][j] != 0:
-            tabuleiroBool[i][j] = False
+    flag2 = True
+    # Ler o arquivo de jogadas e preencher o tabuleiro
+    flag3 = ler_jogadas(arquivo, tabuleiro)
+    if flag3:
+        flag2 = False
+        print("Arquivo de dicas inválido!")
+
+    # Atualizando o tabuleiroBool para marcar as dicas como imutáveis
+    for i in range(9):
+        for j in range(9):
+            if tabuleiro[i][j] != 0:
+                tabuleiroBool[i][j] = False
 
 
+# Verifica se é o modo interativo:
+if len(sys.argv) == 2:
 
-# Interatividade do sistema (Início):
-contador = 0
+    # Interatividade do sistema (Início):
+    contador = 0
+    flag4 = False
 
-while flag2:
+    while flag2:
 
-    if contador == 0:
-        # Exibir o tabuleiro inicial
-        print_tabuleiro(tabuleiro)
-        entrada = input("Entre com uma jogada: ")
-    else:
-        entrada = input("Próxima jogada! ")
-
-    # Condições que implementam o comando de apagar uma célula:
-    if entrada[0] == "!":
-        entrada = entrada.replace("!", "")
-        entrada = entrada.replace(",", " ")
-        coluna, linha = entrada.split()
-        colunaNum = str.lower(coluna)
-        colunaNum = ord(colunaNum) - ord("a")
-        linha = int(linha) - 1
-
-        if linha > 9 or linha < 0 or colunaNum > 9 or colunaNum < 0:
-            print("Jogada no formato inválido!")
-
-        elif tabuleiroBool[linha][colunaNum] and tabuleiro[linha][colunaNum] != 0:
-            simNao = certeza("\nVocê tem certeza que deseja apagar essa célula? ")
-            if simNao == "s":
-                print("\nApagando linha", linha, "coluna", coluna + ".")
-                tabuleiro[linha][colunaNum] = 0
-                print_tabuleiro(tabuleiro)
-            else:
-                print("A célula não foi alterada.")
-        elif not tabuleiroBool[linha][colunaNum]:
-            print("Não é possível apagar uma célula-dica!")
-            
-        else:
-            print("A célula escolhida já é vazia...")
-
-    # Condições que implementam o comando de saber quais números podem ser colocados em uma célula:
-    elif entrada[0] == "?":
-        entrada = entrada.replace("?", "")
-        entrada = entrada.replace(",", " ")
-        coluna, linha = entrada.split()
-        colunaNum = str.lower(coluna)
-        colunaNum = ord(colunaNum) - ord("a")
-        linha = int(linha) - 1
-
-        resposta = []
-        numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-        if not tabuleiroBool[linha][colunaNum]:
-            print("Essa célula é uma dica\n")
-        else:
-            for i in range(9):
-                if check(tabuleiro, colunaNum, linha, numeros[i]):
-                    resposta.append(numeros[i])
-
-            print("\nOs números possíveis para esta célula são:\n")
-            print(resposta)
-            print("\n")
-    
-    # Jogada no formato "<coluna>, <linha> : <numero>":
-    else:
-        entrada = entrada.replace(":", " ")
-        entrada = entrada.replace(",", " ")
-
-        coluna, linha, numero = entrada.split()
-        linha = linha.strip()
-        linha = int(linha) - 1
-        numero = numero.strip()
-        numero = int(numero)
-        coluna = coluna.strip()
-
-        coluna = str.lower(coluna)
-        coluna = ord(coluna) - ord("a")
-
-        if numero > 9 or numero < 1 or linha > 8 or \
-           linha < 0 or coluna > 8 or coluna < 0 or entrada == " ":
-            print("\nJogada no formato inválido!")
-        
-        elif not tabuleiroBool[linha][coluna]:
-            print("\nA jogada é inválida!\nNão se pode substituir um número-dica.")
-
-        elif tabuleiro[linha][coluna] == numero:
-            print("\nA jogada não mudará o tabuleiro...\n")
-        
-        elif not check(tabuleiro, coluna, linha, numero):
-            print("\nA jogada é inválida!\nA jogada fere as regras do jogo.")
-        
-        elif check(tabuleiro, coluna, linha, numero) and tabuleiroBool[linha][coluna] and tabuleiro[linha][coluna] == 0:
-            print("\nJogada válida!\n")
-            tabuleiro[linha][coluna] = numero
+        if contador == 0:
+            # Exibir o tabuleiro inicial
             print_tabuleiro(tabuleiro)
-        
-        elif check(tabuleiro, coluna, linha, numero) and tabuleiroBool[linha][coluna] and tabuleiro[linha][coluna] != 0:
-            simNao = certeza("\nA jogada é válida, mas a célula escolhida já está preenchida, "
-                           "deseja substituí-la? (s/n)\n")
-            if simNao == "s":
-                tabuleiro[linha][coluna] = numero
-                print("\nSubstituindo...\n")
-                print_tabuleiro(tabuleiro)
-            else:
-                print("A célula foi mantida!\n")
+            entrada = input("Entre com uma jogada: ")
+        elif flag4:
+            entrada = input("Tente novamente. ")
+            flag4 = False
+        else:
+            entrada = input("Próxima jogada! ")
+
+        #Padronizando entrada:
+        entrada = entrada.replace(":", "")
+        entrada = entrada.replace(";", "")
+        entrada = entrada.replace(".", "")
+        entrada = entrada.replace(",", "")
+        entrada = entrada.replace(" ", "")
+        entrada = entrada.strip()
+
+        if len(entrada) == 3:
+
+            # Condições que implementam o comando de apagar uma célula:
+            if entrada[0] == "!":
+                coluna, linha = entrada[1], entrada[2]
+                colunaNum = str.lower(coluna)
+                colunaNum = ord(colunaNum) - ord("a")
+                linha = int(linha) - 1
+
+                if linha > 9 or linha < 0 or colunaNum > 9 or colunaNum < 0:
+                    print("Jogada no formato inválido!")
+
+                elif tabuleiroBool[linha][colunaNum] and tabuleiro[linha][colunaNum] != 0:
+                    simNao = certeza("\nVocê tem certeza que deseja apagar essa célula? ")
+                    if simNao == "s":
+                        print("\nApagando linha", linha, "coluna", coluna + ".")
+                        tabuleiro[linha][colunaNum] = 0
+                        print_tabuleiro(tabuleiro)
+                    else:
+                        print("A célula não foi alterada.")
+                elif not tabuleiroBool[linha][colunaNum]:
+                    print("Não é possível apagar uma célula-dica!")
+                    
+                else:
+                    print("A célula escolhida já é vazia...")
+
+            # Condições que implementam o comando de saber quais números podem ser colocados em uma célula:
+            elif entrada[0] == "?":
+                coluna, linha = entrada[1], entrada[2]
+                colunaNum = str.lower(coluna)
+                colunaNum = ord(colunaNum) - ord("a")
+                linha = int(linha) - 1
+
+                resposta = []
+                numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                if not tabuleiroBool[linha][colunaNum]:
+                    print("Essa célula é uma dica\n")
+                else:
+                    for i in range(9):
+                        if check(tabuleiro, colunaNum, linha, numeros[i]):
+                            resposta.append(numeros[i])
+
+                    print("\nOs números possíveis para esta célula são:\n")
+                    print(resposta)
+                    print("\n")
             
+            # Jogada no formato "<coluna>, <linha> : <numero>":
+            else:
+                coluna, linha, numero = entrada[0], entrada[1], entrada[2]
+                linha = int(linha) - 1
+                numero = int(numero)
 
-    i = 0
-    flag2 = False
-    while i < 9 and not flag2:
-        j = 0
-        while j < 9 and not flag2:
-            if tabuleiro[i][j] == 0:    
-                flag2 = True
-            j += 1
-        i += 1
-    contador += 1
+                coluna = str.lower(coluna)
+                coluna = ord(coluna) - ord("a")
 
-print("JOGO CONCLUIDO")
-print("PARABENS!!")
+                if numero > 9 or numero < 1 or linha > 8 or \
+                linha < 0 or coluna > 8 or coluna < 0 or entrada == " ":
+                    print("\nJogada no formato inválido!")
+                
+                elif not tabuleiroBool[linha][coluna]:
+                    print("\nA jogada é inválida!\nNão se pode substituir um número-dica.")
+
+                elif tabuleiro[linha][coluna] == numero:
+                    print("\nA jogada não mudará o tabuleiro...\n")
+                
+                elif not check(tabuleiro, coluna, linha, numero):
+                    print("\nA jogada é inválida!\nA jogada fere as regras do jogo.")
+                
+                elif check(tabuleiro, coluna, linha, numero) and tabuleiroBool[linha][coluna] and tabuleiro[linha][coluna] == 0:
+                    print("\nJogada válida!\n")
+                    tabuleiro[linha][coluna] = numero
+                    print_tabuleiro(tabuleiro)
+                
+                elif check(tabuleiro, coluna, linha, numero) and tabuleiroBool[linha][coluna] and tabuleiro[linha][coluna] != 0:
+                    simNao = certeza("\nA jogada é válida, mas a célula escolhida já está preenchida, "
+                                "deseja substituí-la? (s/n)\n")
+                    if simNao == "s":
+                        tabuleiro[linha][coluna] = numero
+                        print("\nSubstituindo...\n")
+                        print_tabuleiro(tabuleiro)
+                    else:
+                        print("A célula foi mantida!\n")
+                
+        else:
+            print("\nJogada no formato inválido!")
+            flag4 = True
+
+        i = 0
+        flag2 = False
+        while i < 9 and not flag2:
+            j = 0
+            while j < 9 and not flag2:
+                if tabuleiro[i][j] == 0:    
+                    flag2 = True
+                j += 1
+            i += 1
+        contador += 1
+
+    print("JOGO CONCLUIDO")
+    print("PARABENS!!")
+        
+elif len(sys.argv) == 3:
+    # TODO modo batch
+    print("fazer")
 
 # Interatividade do sistema (Fim).
